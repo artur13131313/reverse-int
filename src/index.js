6be72d641389c7a484b71e02d36d5a8fab3303ae
Артур <97834832+artur13131313@users.noreply.github.com>
2022-06-24 10:11:31 +0300

    add new codes 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    const str = (Math.abs(n)).toString()
+    let res = ''
+    const isNegative = n < 0
+    
+    for (let i = 0; i < str.length; i++){
+    const char = str[i]
+    if (char !== '0') res = char + res
+    else if (char === '0' && str[i + 1] && str[i + 1] !== '0') res = char + res
+    }
+
+    return Math.abs (Math.abs (Number(isNegative ? res * -1 : res)))
 }
